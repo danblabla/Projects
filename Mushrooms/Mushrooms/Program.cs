aa82454fbb384fa82a155b5216c2527397417034
danblabla <dan.bla.bla@gmail.com>
2015-10-08 14:14:01 +0300

    fix total %(x+1)

---
+++ Mushrooms/Mushrooms/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mushrooms
+{
+    public class Mushrooms
+    {
+        public static void Main(string[] args)
+        {
+            int red;
+            int white;
+            int total;
+            int x;
+
+            //Console.WriteLine("Nomber of red mushrooms, red= ");
+            //red = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many mushrooms are = ");
+            total = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many red mushrooms are more than white= ");
+            x = int.Parse(Console.ReadLine());
+
+            if (total % (x + 1) == 0)
+            {
+
+                white = total / (x + 1);
+                Console.WriteLine("White mushrooms= " + white);
+
+                red = total - white;
+                Console.WriteLine("Red mushrooms= " + red);
+            }
+            else
+            {
+                Console.WriteLine("Numebers of white and red mushrooms are decimal");
+            }
+            Console.WriteLine("");
+            Console.ReadKey();
+
+        }
+    }
+}
