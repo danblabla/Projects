3caeb6036f65266a557c8af78affebbe7aba7c7c
danblabla <dan.bla.bla@gmail.com>
2015-10-08 16:43:46 +0300

    not done yet

--- BirdAndTrains/BirdAndTrains/Program.cs
+++ BirdAndTrains/BirdAndTrains/Program.cs
@@ -10,10 +10,28 @@ namespace BirdAndTrains
     {
         public static void Main(string[] args)
         {
-            float birdspeed;
-            float trainspeed;
-            float distance;
-            float birddistance;
+            double birdspeed;
+            double trainspeed;
+            double distance;
+            double birddistance;
+
+            Console.WriteLine("Enter trains speed=");
+            trainspeed = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter bird speed=");
+            birdspeed = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter inistial distance=");
+            distance = double.Parse(Console.ReadLine());
+
+            if(trainspeed==0|| trainspeed<0|| birdspeed==0|| birdspeed<0|| distance==0|| distance<0)
+            {
+                Console.WriteLine("Please enter valid values");
+            }
+            else
+            {
+
+            }
         }
     }
 }
