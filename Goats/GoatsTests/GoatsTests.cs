da7e0e4a6c5cc926b49b02ce657c9b59758f0254
danblabla <dan.bla.bla@gmail.com>
2015-10-08 13:57:24 +0300

    goats test

---
+++ Goats/GoatsTests/GoatsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Goats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goats.Tests
+{
+    [TestClass()]
+    public class GoatsTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            float w = 5;
+            int y = 0;
+            string i = "imposible";
+            if (w == 0 || y == 0)
+            {
+                Assert.AreEqual(0, y);
+            }
+        }
+    }
+}
