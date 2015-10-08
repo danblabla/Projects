c91e32c7ea3507c7c01e317fa0ad03645ce10895
danblabla <dan.bla.bla@gmail.com>
2015-10-08 16:29:20 +0300

    prog

---
+++ BirdAndTrains/BirdAndTrains/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdAndTrains
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            float birdspeed;
+            float trainspeed;
+            float distance;
+            float birddistance;
+        }
+    }
+}
