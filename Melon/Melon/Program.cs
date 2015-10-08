f0a85841b56a11aaa29c97695e37cafc2adec528
danblabla <dan.bla.bla@gmail.com>
2015-10-08 13:18:09 +0300

    melon prog

---
+++ Melon/Melon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melon
+{
+    public class Melon
+    {
+        public static void Main(string[] args)
+        {
+            int x;
+            // melon weight x
+
+            Console.WriteLine("Enter melon weight in kg x=");
+            x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0 && x > 2)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+            Console.ReadKey();
+            return;
+        }
+    }
+}
