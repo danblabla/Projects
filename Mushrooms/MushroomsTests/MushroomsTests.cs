8b57c68b2760193e4ad837020d7c5c60be09b867
danblabla <dan.bla.bla@gmail.com>
2015-10-08 14:18:27 +0300

    tests done

---
+++ Mushrooms/MushroomsTests/MushroomsTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mushrooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mushrooms.Tests
+{
+    [TestClass()]
+    public class MushroomsTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            int total = 10;
+            int x = 4;
+            Assert.AreEqual(true, total % (x + 1) == 0);
+        }
+
+        [TestMethod()]
+        public void MainTest1()
+        {
+            //int white = 2;
+            //int red = 8;
+            int total = 10;
+            int x = 4;
+            Assert.AreEqual(2,total/(x+1));
+            Assert.AreEqual(8, total-total/(x+1));
+        }
+    }
+}
