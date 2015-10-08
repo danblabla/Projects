6b21faf574f77e86f0b2beccbbb76b335b10dff8
danblabla <dan.bla.bla@gmail.com>
2015-10-08 15:33:37 +0300

    almost done

---
+++ SportsMan/SportsMan/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsMan
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int N;
+            int x;
+
+            Console.WriteLine("Enter the numeber of reps rounds N=");
+            N = int.Parse(Console.ReadLine());
+           
+          
+                x = (int)Math.Pow(N, 2);
+                Console.WriteLine("Total number of reps is" + x);
+          
+                //Console.WriteLine("The number is not integer");
+           
+           
+          
+               
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
